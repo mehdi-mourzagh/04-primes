f280ce216cb2b0016496fb292be643c967eba04c
mehdi-mourzagh <mehdi.mourzagh@edu.esiee.fr>
2025-09-22 13:41:58 +0000

    Ajout de docstring et de la fonction secondaire

--- main.py
+++ main.py
@@ -1,27 +1,21 @@
+"""Affiche les nombres premiers de 0 à 99."""
 from math import sqrt
 
-#### Fonction secondaire
-
-
-def isprime(p):
-
-    # votre code ici
-
-    pass
-
-#### Fonction principale
-
+def is_prime(p):
+    """Retourne True si p est un nombre premier, False sinon."""
+    if p < 2:
+        return False
+    for i in range(2, int(sqrt(p)) + 1):
+        if p % i == 0:
+            return False
+    return True
 
 def main():
-
-    # vos appels à la fonction secondaire ici
-
+    """Affiche les nombres premiers de 0 à 99 sur une ligne."""
     for n in range(100):
-        if isprime(n):
-            print(n, end=", ")
-
+        if is_prime(n):
+            print(n, end=" ")
     print()
-
 
 if __name__ == "__main__":
     main()
